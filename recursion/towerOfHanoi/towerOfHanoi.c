65e1ea4b1277a4976a3483b323e9610630c6ec68
Rishabh Rathore <rishabh.rathore@bajajfinserv.in>
2024-01-13 15:59:18 +0530

    fixes the c program

--- recursion/towerOfHanoi/towerOfHanoi.c
+++ recursion/towerOfHanoi/towerOfHanoi.c
@@ -1,11 +1,17 @@
-void TowersOHanoi(int n, char frompeg, char topeg, char auxpeg)
-    {
-        if(n == 1) 
-        {
-            printf("Move disk 1 from peg %c to peg %c",frompeg, topeg);
-            return;
-        }
-        TowersOfHanoi(n-1, frompeg, auxpeg, topeg);
-        printf("\n Move disk %d from peg %c to peg %c", n, frompeg, topeg);
-        TowersOfHanoi(n-1, auxpeg, topeg, frompeg);
-    }   
+#include <stdio.h>
+
+void TowersOfHanoi(int n, char frompeg, char topeg, char auxpeg) {
+    if (n == 1) {
+        printf("Move disk 1 from peg %c to peg %c\n", frompeg, topeg);
+        return;
+    }
+    TowersOfHanoi(n - 1, frompeg, auxpeg, topeg);
+    printf("Move disk %d from peg %c to peg %c\n", n, frompeg, topeg);
+    TowersOfHanoi(n - 1, auxpeg, topeg, frompeg);
+}
+
+int main() {
+    int n = 3; // You can change this to the desired number of disks
+    TowersOfHanoi(n, 'A', 'C', 'B');
+    return 0;
+}
